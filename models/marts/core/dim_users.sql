4eae74211e7882f534e5184d4bf2cc8113bfefeb
Antonio Galdo <antoniogs9@hotmail.es>
2026-05-07 14:54:38 +0000

    cast en marts/core/users

--- models/marts/core/dim_users.sql
+++ models/marts/core/dim_users.sql
@@ -26,6 +26,7 @@ addresses as (
 
 ),
 
+
 final as (
 
     select
@@ -34,24 +35,22 @@ final as (
         , u.last_name
         , concat(u.first_name, ' ', u.last_name)             as full_name
         , u.email
-
-        -- Validación de formato de email. Esta columna se valida
-        -- con un unit_test en _core__models.yml.
         , coalesce(
               regexp_like(u.email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$'),
               false
           )                                                  as is_valid_email_address
-
         , u.phone_number
         , u.address_id
         , a.address_line
         , a.zipcode::varchar(20) as zipcode
         , a.state
         , a.country
-        , u.created_at_utc                                   as registered_at_utc
-        , u.updated_at_utc                                   as last_updated_at_utc
-        , datediff('day', u.created_at_utc, current_timestamp()) as days_since_registration
-        , u.date_load
+        -- FORZAMOS EL TIPO DE DATO A TIMESTAMP_TZ PARA CUMPLIR EL CONTRATO:
+        , u.created_at_utc::timestamp_tz                     as registered_at_utc
+        , u.updated_at_utc::timestamp_tz                     as last_updated_at_utc
+        -- AÑADIMOS PRECISIÓN AL NUMBER PARA QUITAR EL WARNING:
+        , datediff('day', u.created_at_utc, current_timestamp())::number(38,0) as days_since_registration
+        , u.date_load::timestamp_tz                          as date_load
     from users u
     left join addresses a on u.address_id = a.address_id
 
